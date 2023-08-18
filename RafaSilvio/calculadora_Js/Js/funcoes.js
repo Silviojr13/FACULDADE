77c7d4b2240bb485cfef8dd30641533e737d4b8d
Silviojr13 <silviomarcaneiro@gmail.com>
2023-08-18 15:41:24 -0300

    s

---
+++ RafaSilvio/calculadora_Js/Js/funcoes.js
@@ -0,0 +1,35 @@
+function somar (){
+    var valor1 = parseInt(document.getElementById("valor1").value);
+    var valor2 = parseInt(document.getElementById("valor2").value);
+
+    var soma = valor1 + valor2;
+
+    alert(soma);
+}
+
+function subtrair (){
+    var subtrair1 = parseInt(document.getElementById("subtrair1").value);
+    var subtrair2 = parseInt(document.getElementById("subtrair2").value);
+
+    var subtrair = subtrair1 - subtrair2;
+
+    alert(subtrair);
+}
+
+function multiplicar (){
+    var multiplicar1 = parseInt(document.getElementById("multiplicar1").value);
+    var multiplicar2 = parseInt(document.getElementById("multiplicar2").value);
+
+    var multiplicar = multiplicar1 * multiplicar2;
+
+    alert(multiplicar);
+}
+
+function dividir (){
+    var dividir1 = parseInt(document.getElementById("dividir1").value);
+    var dividir2 = parseInt(document.getElementById("dividir2").value);
+
+    var dividir = dividir1 / dividir2;
+
+    alert(dividir);
+}
