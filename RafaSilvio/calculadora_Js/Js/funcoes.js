db85b7f4a91897d4fff4745af3d53d457672d89b
Silviojr13 <silviomarcaneiro@gmail.com>
2023-08-19 02:23:50 -0300

    s

--- RafaSilvio/calculadora_Js/Js/funcoes.js
+++ RafaSilvio/calculadora_Js/Js/funcoes.js
@@ -1,10 +1,11 @@
 function somar (){
     var valor1 = parseInt(document.getElementById("valor1").value);
     var valor2 = parseInt(document.getElementById("valor2").value);
+    var resultOP1 = document.getElementById("resutadoOp1");
 
     var soma = valor1 + valor2;
 
-    alert(soma);
+    resultOP1.value = soma;
 }
 
 function subtrair (){
