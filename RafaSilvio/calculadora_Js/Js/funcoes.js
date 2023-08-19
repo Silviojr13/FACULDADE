d57a440b0e36482e8daf32b4479b4a14a0b77469
Silviojr13 <silviomarcaneiro@gmail.com>
2023-08-19 02:43:37 -0300

    s

--- RafaSilvio/calculadora_Js/Js/funcoes.js
+++ RafaSilvio/calculadora_Js/Js/funcoes.js
@@ -1,6 +1,10 @@
 function somar (){
+
     var valor1 = parseInt(document.getElementById("valor1").value);
     var valor2 = parseInt(document.getElementById("valor2").value);
+
+    /* na linha 7 a variavel resultOP1 é responsavel por dar um print na tela do resultado*/
+
     var resultOP1 = document.getElementById("resutadoOp1");
 
     var soma = valor1 + valor2;
@@ -12,25 +16,31 @@ function subtrair (){
     var subtrair1 = parseInt(document.getElementById("subtrair1").value);
     var subtrair2 = parseInt(document.getElementById("subtrair2").value);
 
+    var resultOP2 = document.getElementById("resutadoOp1");
+
     var subtrair = subtrair1 - subtrair2;
 
-    alert(subtrair);
+    resultOP2.value = subtrair
 }
 
 function multiplicar (){
     var multiplicar1 = parseInt(document.getElementById("multiplicar1").value);
     var multiplicar2 = parseInt(document.getElementById("multiplicar2").value);
 
+    var resultOP3 = document.getElementById("resutadoOp1");
+
     var multiplicar = multiplicar1 * multiplicar2;
 
-    alert(multiplicar);
+    resultOP3.value = multiplicar
 }
 
 function dividir (){
     var dividir1 = parseInt(document.getElementById("dividir1").value);
     var dividir2 = parseInt(document.getElementById("dividir2").value);
 
+    var resultOP4 = document.getElementById("resutadoOp1");
+
     var dividir = dividir1 / dividir2;
 
-    alert(dividir);
+    resultOP4.value = dividir
 }
