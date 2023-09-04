cc4b5b67e590a1e0b6e67bc5f5e751af09759d96
Silviojr13 <silviomarcaneiro@gmail.com>
2023-09-04 15:49:09 -0300

    s

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
-    fetch("/html_import/menu.html")
-      .then(response => response.text())
-      .then(data => {
-        const menuPlaceholder = document.getElementById("menu-placeholder");
-        menuPlaceholder.innerHTML = data;
-      })
-      .catch(error => {
-        console.error("Erro ao carregar o menu:", error);
-      });
-  });
+  fetch("/html_import/menu.html")
+    .then((response) => response.text())
+    .then((data) => {
+      const menuPlaceholder = document.getElementById("menu-placeholder");
+      menuPlaceholder.innerHTML = data;
+    })
+    .catch((error) => {
+      console.error("Erro ao carregar o menu:", error);
+    });
+});
+
+
